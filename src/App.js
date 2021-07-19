05720faf31c1841817a92fa72a3d8800f7e83520
defauth98 <neto.daniribeiro@gmail.com>
2021-07-19 09:57:29 -0300

    fix: removendo os exact path

--- src/App.js
+++ src/App.js
@@ -14,14 +14,12 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ MovieList } />
-        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
         <Route
-          exact
           path="/movies/:id"
           render={ (props) => <MovieDetails { ...props } /> }
         />
         <Route
-          exact
           path="/movies/:id/edit"
           render={ (props) => <EditMovie { ...props } /> }
         />
