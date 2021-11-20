cef579391394863d6e1f9510aca91444098509c7
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2021-11-20 11:06:37 -0300

    Updates

---
+++ precache-manifest.862c068fe9ba7ed9eaa2b7fb2014eda4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71089041f96587eb0468ce34632d2a8c",
+    "url": "/movie-cards-library-crud/index.html"
+  },
+  {
+    "revision": "5eb895c09ab3cac1899c",
+    "url": "/movie-cards-library-crud/static/css/main.d37623a0.chunk.css"
+  },
+  {
+    "revision": "069d81ed1204e5196065",
+    "url": "/movie-cards-library-crud/static/js/2.303e59f0.chunk.js"
+  },
+  {
+    "revision": "5eb895c09ab3cac1899c",
+    "url": "/movie-cards-library-crud/static/js/main.8f277199.chunk.js"
+  },
+  {
+    "revision": "41cd13ef2e36180001c8",
+    "url": "/movie-cards-library-crud/static/js/runtime-main.d37bb6d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-cards-library-crud/precache-manifest.b3b5475711f39896add36a0bac744884.js"
+  "/movie-cards-library-crud/precache-manifest.862c068fe9ba7ed9eaa2b7fb2014eda4.js"
 );
 
 self.addEventListener('message', (event) => {
